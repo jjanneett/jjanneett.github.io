9f60dffcff79a6f687e25f3c8836de4f34cfea9a
jannet <yourmail@gmail.com>
2025-06-27 13:57:17 +0900

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+// 👇 BrowserRouter 대신 HashRouter import!
+import { HashRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
+        {/* 👇 BrowserRouter → HashRouter로 교체 */}
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
