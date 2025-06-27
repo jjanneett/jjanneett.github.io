df4353c9b174f906e6f9b942ae5190bb06c2aa0b
jannet <yourmail@gmail.com>
2025-06-27 14:08:03 +0900

    5st commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { vitePluginGhPages } from 'vite-plugin-gh-pages'
 
 export default defineConfig({
-  plugins: [react(), vitePluginGhPages()]
+  plugins: [react()]
 })
